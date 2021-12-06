2eac343c02c0d81583a92b37d249eeb6a8882984
Jacob Hammontree <jacobhammontree@gmail.com>
2021-12-06 07:46:45 -0500

    did it another way as well, made more extensible

--- python/day6/day6.py
+++ python/day6/day6.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
+from copy import deepcopy
 
-def progress_one_day(fish=defaultdict(lambda:0)):
+def progress_one_day(fish=defaultdict(lambda:0), n=8):
     new_zero = fish[1]
     new_one = fish[2]
     new_two = fish[3]
@@ -22,16 +23,33 @@ def progress_one_day(fish=defaultdict(lambda:0)):
     fish[8] = new_eight
     return fish
 
+# Same as above, but a bit more extensible with looping
+def progress_one_day_2(fish=defaultdict(lambda:0), n=8):
+    n_zero = fish[0]
+    for i in range(0,n+1):
+        if i == n:
+            fish[i] = n_zero
+        elif i == 6:
+            fish[i] = fish[7]+n_zero
+        else:
+            fish[i] = fish[i+1]
+    return fish
+
 if __name__ == "__main__":
     fish_d = defaultdict(lambda:0)
     with open("./python/day6/day6_input.txt", "r") as f:
         for l in f.readlines():
             for x in [int(x) for x in l.split(",")]:
                 fish_d[x]+=1
-    for i in range(500):
+    fish_d_cp = deepcopy(fish_d)
+    for i in range(256):
         progress_one_day(fish_d)
+        progress_one_day_2(fish_d_cp)
     count = 0
     for _,v in fish_d.items():
         count+=v
+    count2 = 0
+    for _,v in fish_d_cp.items():
+        count2+=v
     print(count)
-
+    print(count2)
